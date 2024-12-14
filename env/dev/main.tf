39af697adb58a82bc44db23572c5833418f4f95b
Ivan Junior <ivan_junior_we@hotmail.com>
2024-12-13 22:51:58 -0300

    Alterando de grupoSeguranca para environment

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,8 +1,9 @@
 module "aws-dev" {
-  source         = "../../infra"
-  key_name       = var.key_name
-  path_key       = var.path_key
-  grupoSeguranca = "dev"
-  max_size       = 3
-  min_size       = 1
+  source      = "../../infra"
+  key_name    = var.key_name
+  path_key    = var.path_key
+  environment = "dev"
+  max_size    = 3
+  min_size    = 1
+  prod        = false
 }
