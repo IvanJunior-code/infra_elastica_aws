6527a69a57c6af8863a4f4e0437134fcd72dec6b
Ivan Junior <ivan_junior_we@hotmail.com>
2024-12-13 22:15:38 -0300

    Alterando nome da variável de grupoSeguranca para environment

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -2,7 +2,8 @@ module "aws-prod" {
     source = "../../infra"
     key_name = var.key_name
     path_key = var.path_key
-    grupoSeguranca = "producao"
+    environment = "producao"
     max_size = 10
     min_size = 2
+    prod = true
 }
