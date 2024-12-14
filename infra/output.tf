136a0d14fd4cc63f4927093252e47df074904d8c
Ivan Junior <ivan_junior_we@hotmail.com>
2024-12-13 22:11:46 -0300

    Adicionando string de conexao das instancias caso não seja de prod, e mostrando o loadbalancer apenas se for prod

--- infra/output.tf
+++ infra/output.tf
@@ -1,3 +1,15 @@
 output "access_load_balancer_address" {
-  value = "http://${aws_lb.load_balancer.dns_name}"
+  value = var.prod ? "http://${aws_lb.load_balancer[0].dns_name}" : ""
+}
+
+
+data "aws_instances" "instances" {
+  filter {
+    name   = "instance-type"
+    values = [aws_launch_template.template_maquina.instance_type]
+  }
+}
+
+output "ssh_string_connection" {
+  value = var.prod ? [] : [for instance in data.aws_instances.instances.public_ips : "ssh -i ${var.path_key}${var.key_name}.pem ubuntu@${instance}"]
 }
