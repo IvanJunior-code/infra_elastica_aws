e2372b099d1842e697584dfe89758d0a74a74feb
Ivan Junior <ivan_junior_we@hotmail.com>
2024-12-13 22:49:37 -0300

    Adicionando regra de liberação de acesso a porta 80 (http) apenas para prod, e para não prod adicionada a regra permitindo acesso ssh

--- infra/security_groups.tf
+++ infra/security_groups.tf
@@ -1,15 +1,16 @@
 resource "aws_security_group" "security_group" {
-  name = var.grupoSeguranca
+  name = var.environment
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress_rule_enable_all" {
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_dev_ssh" {
+  count             = var.prod ? 0 : 1
   security_group_id = aws_security_group.security_group.id
 
   cidr_ipv4 = "0.0.0.0/0"
   #cidr_ipv6   = "::/0"
-  #from_port   = 0
-  ip_protocol = "-1"
-  #to_port     = 0
+  from_port   = 22
+  ip_protocol = "TCP"
+  to_port     = 22
 
   tags = {
     Name = "Ingress Rule"
@@ -21,11 +22,24 @@ resource "aws_vpc_security_group_egress_rule" "egress_rule_enable_all" {
 
   cidr_ipv4 = "0.0.0.0/0"
   #cidr_ipv6   = "::/0"
-  #from_port   = 0
   ip_protocol = "-1"
-  #to_port     = 0
 
   tags = {
     Name = "Egress Rule"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule_prod_http" {
+  count             = var.prod ? 1 : 0
+  security_group_id = aws_security_group.security_group.id
+
+  cidr_ipv4 = "0.0.0.0/0"
+  #cidr_ipv6   = "::/0"
+  from_port   = 80
+  ip_protocol = "TCP"
+  to_port     = 80
+
+  tags = {
+    Name = "Ingress Rule"
   }
 }
