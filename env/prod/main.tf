a10b2573cff45e2387c0751bc0904d2388194bbb
Ivan Junior <ivan_junior_we@hotmail.com>
2024-12-15 20:49:29 -0300

    Ajustando nome do ambiente

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -2,7 +2,7 @@ module "aws-prod" {
   source      = "../../infra"
   key_name    = var.key_name
   path_key    = var.path_key
-  environment = "producao"
+  environment = "PRODUCAO"
   max_size    = 10
   min_size    = 2
   prod        = true
